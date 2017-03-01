0c8ce55b8426c2c59f197c7972f43ee563886cdb
Jon Burk <jon@burk.com>
2017-03-01 07:15:58 -0500

    Moved routing to a module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SearchComponent } from './recipes/search.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search',  component: SearchComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { SearchComponent } from './recipes/search.component';
+
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/search',
-        pathMatch: 'full'
-      },
-      {
-        path: 'search',
-        component: SearchComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
