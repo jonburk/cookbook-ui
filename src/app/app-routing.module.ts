878865294173c73ff9335e2d1eacd471c0b1efce
Jonathan Burk <jburk@foliage.com>
2017-06-22 14:00:32 -0400

    Implemented view recipe page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { SearchComponent } from './recipes/search.component'
+import { ViewRecipeComponent } from './recipes/view.component'
+import { ViewRecipeResolver } from './recipes/view.resolver'
+import { RecipeService } from './recipes/recipe.service'
 
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full' },
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'recipes/:id', component: ViewRecipeComponent, resolve: { recipe: ViewRecipeResolver } }
 ]
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [ ViewRecipeResolver, RecipeService ]
 })
 
 export class AppRoutingModule {}
---
+++ src/app/recipes/view.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core'
+import { OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+
+import { Recipe } from '../shared/recipe'
+
+@Component({
+  selector: 'cb-view-recipe',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss']
+})
+
+export class ViewRecipeComponent implements OnInit {
+  recipe: Recipe
+
+  constructor (private route: ActivatedRoute) { }
+
+  ngOnInit (): void {
+    this.route.data.subscribe((data: { recipe: Recipe }) => {
+      this.recipe = data.recipe
+    })
+  }
+
+  getIngredients (): string {
+    return this.recipe.ingredients.map(i => i.name).join(', ')
+  }
+
+  getPicture () {
+    return `http://localhost:8080/api/recipes/${this.recipe.id}/image`
+  }
+}
